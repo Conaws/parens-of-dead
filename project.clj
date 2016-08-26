7d72af3fdc88af6114fcc6b1b46ac79618becaf3
Conor White-Sullivan <cwhitesullivan@gmail.com>
2016-08-27 01:52:50 +0530

    Blank project clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject parens-of-the-dead "0.1.0-SNAPSHOT"
+  :description "zombie game in cljs and clj"
+  :url ""
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/gol.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:plugins []
+                   :dependencies []
+                   :source-path ["dev"]}})
+
