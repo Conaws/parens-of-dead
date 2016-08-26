977952e7cab6254cc6809c6e21a742fe0d13c356
Conor White-Sullivan <cwhitesullivan@gmail.com>
2016-08-27 04:29:55 +0530

    changed clojure version and added cljs

--- project.clj
+++ project.clj
@@ -5,10 +5,11 @@
             :url "http://www.gnu.org/licenses/gol.html"}
   :jvm-apts ["-XX:MaxPermSize-256m"]
   :main undead.system
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.1.18"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
+                 [org.clojure/clojurescript "1.9.225"]
                  [com.stuartsierra/component "0.3.1"]
                  [reloaded.repl "0.2.2"]
+                 [http-kit "2.1.18"]
                  [compojure "1.5.1"]]
   :profiles {:dev {:plugins []
                    :dependencies []
