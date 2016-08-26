2f73d81c306c12a7f61c6939ce31a46e39fc2971
Conor White-Sullivan <cwhitesullivan@gmail.com>
2016-08-27 04:32:08 +0530

    reloaded goes in dev profile

--- project.clj
+++ project.clj
@@ -8,10 +8,9 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
                  [org.clojure/clojurescript "1.9.225"]
                  [com.stuartsierra/component "0.3.1"]
-                 [reloaded.repl "0.2.2"]
                  [http-kit "2.1.18"]
                  [compojure "1.5.1"]]
   :profiles {:dev {:plugins []
-                   :dependencies []
+                   :dependencies [[reloaded.repl "0.2.2"]]
                    :source-paths ["dev"]}})
 
