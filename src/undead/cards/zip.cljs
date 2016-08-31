21233afb713aee8b4917bf8bd93e2f68d95eaf10
Conor White-Sullivan <cwhitesullivan@gmail.com>
2016-08-31 07:59:54 +0530

    Got a basic walking system going
    still might have some bugs around left and right

--- src/undead/cards/zip.cljs
+++ src/undead/cards/zip.cljs
@@ -6,6 +6,7 @@
             [reagent.core :as r])
   (:require-macros [devcards.core :refer [defcard-rg]]))
 
+
 (defcard-rg test
   [:div "hello"])
 
@@ -23,16 +24,16 @@
              :children/order [2 3]}
             {:db/id 2
              :node/text "Node 2"
-             :children/order [4 3]}
+             :children/order [4]}
             {:db/id 3
              :node/text "Node 3"
-             :children/order [2 5]}
+             :children/order [5]}
             {:db/id 4
              :node/text "Node 4"
-             :children/order [1]}
+            }
             {:db/id 5
              :node/text "Node 5"
-             :children/order [1]}
+             }
 
             ])
 
@@ -71,34 +72,54 @@
       "up"]
      [:button {:on-click #(swap! zatom z/down)}
       "down"]
+     [:button {:on-click #(swap! zatom z/right)}
+      "right"]
+     [:button {:on-click #(swap! zatom z/left)}
+      "left"]
      [:button {:on-click #(swap! zatom z/remove)}
       "remove"]
-     [:button {:on-click (fn [e]
-                           (swap! zatom
-                                  (fn [z]
-                                    (z/insert-child z 1))))}
-      "insert-child 1"]
-     [:button {:on-click (fn [e]
-                           (swap! zatom
-                                  (fn [z]
-                                    (z/append-child z 1))))}
-      "append-child 1"]]
+     ]
     ))
 
+(defonce zatom1 (r/atom (ds-zip conn 1)))
 
-
-(defn ds-zip-view [conn]
-  (let [zatom (r/atom (ds-zip conn 1))]
-    (fn []
+(defn ds-zip-view [zatom]
+    (fn [zatom]
       [:div
        [z-buttons zatom]
        (pr-str @zatom)
-       ])))
-
+       (let [[n zstruc :as z] @zatom]
+         (if zstruc
+           [:div {:style {:display "grid"
+                          :background-color "blue"
+                          :grid-column-gap "60px"
+                          :grid-row-gap "60px"
+                          :grid-template-rows "120px 120px 120px"
+                          :grid-template-areas "'.. parent .. '
+'left node right'
+'.. children ..'
+"
+                          }}
+            [:div {:style {:grid-area "parent"}}
+             (pr-str (:pnodes zstruc))
+             ]
+            [:div {:style {:grid-area "left"}}
+             (pr-str (:l zstruc))]
+            [:div.cell {:style {:grid-area "node"}}
+             (pr-str n)] 
+            [:div {:style {:grid-area "right"}}
+             (pr-str (:r zstruc))]
+            [:div {:style {:grid-area "children"}}
+             (pr-str (z/next z))]])
+         )
+       ]))
 
 
 (defcard-rg nodea
-  [ds-zip-view conn])
+  [ds-zip-view zatom1]
+  zatom1
+  {:inspect-data true
+   :history true})
 
 
 ;(pprint (get-node conn 1))
