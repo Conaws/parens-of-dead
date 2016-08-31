87ddc11aad8d84eaae911c8b2ea383faab7af728
Conor White-Sullivan <cwhitesullivan@gmail.com>
2016-08-31 07:22:01 +0530

    got some sort of tree wor

--- src/undead/cards/zip.cljs
+++ src/undead/cards/zip.cljs
@@ -2,7 +2,8 @@
   (:require [clojure.pprint :refer [pprint]]
             [clojure.zip :as z]
             [datascript.core :as d]
-            [posh.reagent :as posh :refer [posh!]])
+            [posh.reagent :as posh :refer [posh!]]
+            [reagent.core :as r])
   (:require-macros [devcards.core :refer [defcard-rg]]))
 
 (defcard-rg test
@@ -22,10 +23,18 @@
              :children/order [2 3]}
             {:db/id 2
              :node/text "Node 2"
-             :children/order [2 3]}
+             :children/order [4 3]}
             {:db/id 3
              :node/text "Node 3"
-             :children/order [2 3]}])
+             :children/order [2 5]}
+            {:db/id 4
+             :node/text "Node 4"
+             :children/order [1]}
+            {:db/id 5
+             :node/text "Node 5"
+             :children/order [1]}
+
+            ])
 
 
 (d/transact! conn nodes)
@@ -49,9 +58,47 @@
    (get-node db id)))
 
 
+
+
+(defn z-buttons [zatom]
+  (fn [zatom]
+    [:div
+     [:button {:on-click #(swap! zatom z/next)}
+      "next"]
+     [:button {:on-click #(swap! zatom z/prev)}
+      "prev"]
+     [:button {:on-click #(swap! zatom z/up)}
+      "up"]
+     [:button {:on-click #(swap! zatom z/down)}
+      "down"]
+     [:button {:on-click #(swap! zatom z/remove)}
+      "remove"]
+     [:button {:on-click (fn [e]
+                           (swap! zatom
+                                  (fn [z]
+                                    (z/insert-child z 1))))}
+      "insert-child 1"]
+     [:button {:on-click (fn [e]
+                           (swap! zatom
+                                  (fn [z]
+                                    (z/append-child z 1))))}
+      "append-child 1"]]
+    ))
+
+
+
+(defn ds-zip-view [conn]
+  (let [zatom (r/atom (ds-zip conn 1))]
+    (fn []
+      [:div
+       [z-buttons zatom]
+       (pr-str @zatom)
+       ])))
+
+
+
 (defcard-rg nodea
-  [:div
-   (pr-str (ds-zip conn 1))])
+  [ds-zip-view conn])
 
 
 ;(pprint (get-node conn 1))
