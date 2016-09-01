c8b47639a4368ed189d8dc5a2efb4a6ccebe1c23
Conor White-Sullivan <cwhitesullivan@gmail.com>
2016-09-01 14:10:37 +0530

    jquery sortable

--- src/undead/cards/auto.cljs
+++ src/undead/cards/auto.cljs
@@ -51,3 +51,66 @@
    [home]
    [home]
    [home]])
+
+(defn portlet-toggle []
+  [:span.ui-icon.ui-icon-minusthick.portlet-toggle])
+
+(defn portlet-header [title]
+  [:div.portlet-header.ui-widget-header.ui-corner-all
+   [portlet-toggle]
+   title]
+  )
+
+(defn sortable-render []
+    [:div {:style {:height "700px"}}
+     [:div.column
+      [:div.portlet.ui-widget.ui-widget-content.ui-helper-clearfix.ui-corner-all
+       [portlet-header "Feeds"]
+       [:div.portlet-content
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"]]
+      [:div.portlet
+       [portlet-toggle]
+       [:div.portlet-header "News"]
+       [:div.portlet-content "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"]] ]
+
+     [:div.column
+      [:div.portlet
+       [portlet-toggle]
+       [portlet-header "Shopping"]
+       [:div.portlet-content "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"]] ] 
+
+     [:div.column
+      [:div.portlet
+       [portlet-header "Links"]
+       [portlet-toggle]
+       [:div.portlet-content "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"]]
+      [:div.portlet
+       [portlet-header "Images"]
+       [portlet-toggle]
+       [:div.portlet-content "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"]] ]
+     [:div.column]
+     [:div.column]
+     ])
+
+
+
+(defcard-rg sortable-render
+  [sortable-render])
+
+
+(defn sortable-did-mount []
+  (js/$ (fn []
+          (.sortable (js/$ ".column") (clj->js {:connectWith ".column"
+                                                :handle ".portlet-header"
+                                                :cancel ".portlet-toggle"
+                                                :placeholder "portlet-placeholder ui-corner-all"})))))
+
+
+
+(defn sortable []
+  (r/create-class {:reagent-render sortable-render
+                   :component-did-mount sortable-did-mount}))
+
+(defcard-rg sortable
+  [sortable])
+
