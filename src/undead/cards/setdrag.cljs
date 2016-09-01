b288d6095e94d3cab8f7c7b3b710bf352cd51d39
Conor White-Sullivan <cwhitesullivan@gmail.com>
2016-09-02 05:28:27 +0530

    draggable ui, not dropable yet

--- src/undead/cards/setdrag.cljs
+++ src/undead/cards/setdrag.cljs
@@ -5,96 +5,60 @@
 
 
 
-(defn draggable-render []
-  [:div.ui-widget-content {:style {:width "100px"
-                                   :height "100px" 
-                                   :padding "0.5em"
-                                   :float "left" 
-                                   :margin "10px 10px 10px 0"}}
-   [:p "Drag me to my target"]])
+(defn placeholder [ s]
+  [:li {:style {:background "rgb(255,240,120)"}
 
-(defn draggable-did-mount [this]
-  (.draggable (js/$ (r/dom-node this))))
+             :on-drag-leave (fn [e]
+                           (swap! s assoc :over false)
+                           ) 
 
-(defn draggable []
-  (r/create-class {:reagent-render draggable-render
-                   :component-did-mount draggable-did-mount}))
+              
+        }
+   "Place here"]
+  )
 
 
 
-(def app-state (r/atom {:drop-area {:class "ui-widget-header"
-                                    :text "Drop here"}}))
-
-
-
-(defn drop-area-render []
-  (let [class (get-in @app-state [:drop-area :class])
-        text (get-in @app-state [:drop-area :text])]
-    [:div {:class class
-           :style {:width "150px" 
-                   :height "150px"
-                   :padding "0.5em" 
-                   :float "left" 
-                   :margin "10px"}}
-     [:p text]]))
-
-
-
-(defn drop-area-did-mount [this]
-  (.droppable (js/$ (r/dom-node this))
-              #js {:drop (fn []
-                           (swap! app-state assoc-in [:drop-area :class] "ui-widget-header ui-state-highlight")
-                           (swap! app-state assoc-in [:drop-area :text] "Dropped!"))}))
-
-
-
-(defn drop-area []
-  (r/create-class {:reagent-render drop-area-render
-                   :component-did-mount drop-area-did-mount}))
-
-
-(defcard-rg dropable
-  [:div.tall
-   [draggable]
-   [drop-area]])
+(defn listitem [i]
+  (let [s (r/atom {:over false
+                   :dragging false})]
+    (fn [i]
+      (if (:over @s)
+        [placeholder s]
+        [:li {:data-id i
+              :draggable true
+              :style {:display
+                      (if (or (:over @s)(:dragging @s))
+                        "none")
+                      :background-color
+                      (if (:dragging @s)
+                        "green"
+                        "blue")
+                      }
+              :on-drag-enter (fn [e]
+                               (swap! s assoc :over true)
+                               )
+              :on-drag-start (fn [e]
+                               (swap! s assoc :dragging true)
+                               )
+              :on-drag-end (fn [e]
+                             (swap! s assoc :dragging false)
+                             (js/console.log e)
+                             )}
+         i]))) )
 
 
 
 
 
-(defn- select-did-mount [this]
-  (.selectable (js/$ (r/dom-node this))
-               (clj->js {:start #(js/console.log "yo")
-                         :selecting (fn [e ui] (js/console.log ui))})
-               ))
+(defn list-render [l]
+  (let [state (r/atom (vec l))]
+    (fn [l]
+      [:ol
 
+       (for [i l]
+        ^{:key i}[listitem i])])))
 
+(defcard-rg listcard
+  [list-render (range 10)])
 
-
-(defn select- [render-fn]
-  (r/create-class {:reagent-render      render-fn
-                   :component-did-mount select-did-mount}))
-
-
-(defn drag- [f]
-  (r/create-class {:reagent-render f
-                   :component-did-mount draggable-did-mount}))
-
-
-
-(defn select2-render []
-  [:ol#selectable
-   (for [n (range 20)]
-     ^{:key n}[drag- (fn [] [:li n])])])
-
-
-
-(defcard-rg selectable
-  [select- select2-render])
-
-
-
-(defcard-rg buttondrag
-  [:div
-   [:button.tall {:on-drag-enter #(js/alert "wooohhhh")}]
-   [:button {:draggable true} "woah"]])
