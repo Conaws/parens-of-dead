a31b4d5b78ecbd972e7429c204ec0f21b8a728e2
Conor White-Sullivan <cwhitesullivan@gmail.com>
2016-12-14 19:33:08 +0530

    Problem 34 - Iterate

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,44 @@
 (ns user
   (:require [reloaded.repl :refer [reset stop system]]
-            [undead.system]))
+            [undead.system]
+            [clojure.string :as str]))
 
 (reloaded.repl/set-init! #'undead.system/create-system)
+
+
+
+;; fclojure http://www.4clojure.com/problem/solutions/29
+(+ 1 1)
+(defn x [x]
+  (apply str 
+         (filter #(re-matches #"[A-Z]" (str %)) x)))
+
+
+(defn y [x]
+  (reduce str (filter #(Character/isUpperCase %) x)))
+
+
+(defn z [x]
+  (apply str (re-seq #"[A-Z]" x)))
+
+(x "aaa DDDelkD Bb")
+(z "aaa DDDelkD Bb")
+
+
+;; implement range
+
+(let [x (fn [start end]
+          (apply list (loop [acc [] n start]
+                  (if (= end n)
+                    acc
+                    (recur (conj acc n) (inc n))
+                    ))))
+
+      b (fn [s e]
+          (take-while #(< % e) (iterate inc s)))
+      ]
+  (b 1 40))
+
+
+
+
